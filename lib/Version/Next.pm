ca689ea40a02d623cd64408c103117d1921b2f90
David Golden <xdg@xdg.me>
2016-02-25 10:58:58 -0500

    Modernize repo layout

--- lib/Version/Next.pm
+++ lib/Version/Next.pm
@@ -3,7 +3,8 @@ use warnings;
 
 package Version::Next;
 # ABSTRACT: increment module version numbers simply and correctly
-# VERSION
+
+our $VERSION = "0.005";
 
 # Dependencies
 use version 0.81 ();
